b051a170eaf5fa62ad1bc8654b98d0e431f7fc0c
Rubén Valls <r.vallsaparici@gmail.com>
2026-03-30 17:56:00 +0200

    addec correct prices id

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -1,6 +1,6 @@
 export const STRIPE_PRICE_IDS = {
-  PLUS: "price_plus_monthly",
-  PRO: "price_pros_monthly",
+  PLUS: "price_1TGi5iFGfcWVbr5C8izKb2fu",
+  PRO: "price_1TGi6SFGfcWVbr5CDHsZou8p",
 } as const;
 
 export type PriceIdKey = keyof typeof STRIPE_PRICE_IDS;
